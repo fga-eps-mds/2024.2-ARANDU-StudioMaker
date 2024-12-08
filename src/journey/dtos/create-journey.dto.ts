2b77e82305d64eb602bd2f07cd98f8bcc788a43e
gabrielm2q <mariano.gabriel2q@gmail.com>
2024-12-08 14:55:31 -0300

    feat(fga-eps-mds/2024.2-ARANDU-DOC#95): add journey swagger specs
    Co-authored-by: dartmol203 <andre.correa.silva203@gmail.com>

--- src/journey/dtos/create-journey.dto.ts
+++ src/journey/dtos/create-journey.dto.ts
@@ -1,13 +1,26 @@
-import { IsString, IsOptional, IsMongoId } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsMongoId, IsOptional, IsString } from 'class-validator';
 
 export class CreateJourneyDto {
+  @ApiProperty({
+    example: 'Título',
+    required: true
+  })
   @IsString()
   title: string;
 
+  @ApiProperty({
+    example: 'Descrição',
+    required: false
+  })
   @IsOptional()
   @IsString()
   description: string;
 
+  @ApiProperty({
+    example: 'id-do-point',
+    required: false
+  })
   @IsOptional()
   @IsMongoId()
   pointId?: string;
